92ecb4b8c074307d588c34e8a124ba3c97be777a
Phil Azar <azar.phil@gmail.com>
2018-04-01 10:56:33 -0500

    senate network plot

--- R/02_explore.R
+++ R/02_explore.R
@@ -2,9 +2,23 @@
 # Explore & Viz Data   
 ######################
 
+install.packages('igraph')
+install.packages('statnet')
+install.packages('ggiraph')
+install.packages('ggnetwork')
+install.packages('ggrepel')
+library(ggrepel)
+library(ggiraph)
+library(ggnetwork)
+library(igraph)
+library(statnet)
+library(tidyverse)
+library(stringr)
 
-nrow(edges_list[[1]])
-
+setwd('/Users/acazar/Desktop/blog/projects/congress_twitter')
+edges_df <- readRDS('./data/edges_df.rds')
+politicians <- read_csv('./data/politicians.csv')
+pvi <- read_csv('./data/cook_pvi.csv')
 #longer tenure = more connections?
 #closeness between states
 #closeness within states
@@ -16,6 +30,9 @@ nrow(edges_list[[1]])
 # who is the bridge between each chamber, each party
 # 2018 midterms: more competitive seat breed more bipartisanship (following)
 
+######################
+#  LIST -> DF 
+######################
 
 edges_df <- data.frame()
   
@@ -23,6 +40,101 @@ edges_df <- data.frame()
 edges_df <- dplyr::bind_rows(edges_df,edges_list[[i]])  
   }
 
+######################
+# Break into House and Senate
+# Break into Parties
+# Within States and Across States
+######################
+
+politicians$name <- str_c(politicians$first_name, politicians$last_name, sep= " ")
+
+head(edges_df)
+
 edges_df %>% 
-  dplyr::filter(stringr::str_detect(source,'Massie')) %>% 
-  head(10)
+  dplyr::filter(source == 'Mike Bost') %>% 
+  head()
+# add to edges DF for target + source:
+# party, state, S or H, PVI? 
+
+# main network 
+# source party, type
+# connection: dotted if cross party, solid if inter party 
+# size = centrality 
+
+main_df <- edges_df %>% 
+  inner_join(., politicians, by = c('source'='name')) %>% 
+  select(source, target, source_type =type, source_party =party) %>% 
+  inner_join(.,politicians, by=c('target'='name')) %>% 
+  select(source, target, source_type, source_party,target_type =type,target_party=party) %>% 
+  mutate(line = ifelse(source_party == target_party, "solid", 'dotted')) %>% 
+  select(source, target, type=source_type, party=source_party, line)
+
+senate_df <- edges_df %>% 
+  inner_join(., politicians, by = c('source'='name')) %>% 
+  select(source, target, source_type =type, source_party =party) %>% 
+  inner_join(.,politicians, by=c('target'='name')) %>% 
+  select(source, target, source_type, source_party,target_type =type,target_party=party) %>%
+  dplyr::filter(source_type== 'sen' & target_type =='sen') %>% 
+  mutate(line = ifelse(source_party == target_party, "solid", 'dotted')) %>% 
+  select(source, target, type=source_type, party=source_party, line)
+##### igraph to ggnetwork
+View(senate_df)
+senate_df <- main_df %>% 
+            dplyr::filter(type=='sen')
+
+graph_df <- graph_from_data_frame(main_df)
+senate_graph_obj <- graph_from_data_frame(senate_df,directed = FALSE)
+
+head(main_df)
+n <- network::network(edges_df)
+summary.network(n)
+network.vertex.names(n)
+
+set.network.attribute(n,'type', main_df[,'type'])
+set.network.attribute(n, 'party', main_df[,'party'])
+set.network.attribute(n, 'line', main_df[,'line'])
+
+network_df <- ggnetwork(graph_df, layout= 'fruchtermanreingold', cell.jitter=.5 )
+senate_network <- ggnetwork(senate_graph_obj, layout= 'fruchtermanreingold', cell.jitter=.5)
+View(network_df)
+ggplot(network_df, aes(x=x, y=y, xend=xend, yend=yend, fill=party))+
+  geom_edges(aes(linetype=line),color = 'grey50') +
+  theme_minimal() +
+  geom_nodetext(aes(label =vertex.names)) +
+  geom_point_interactive(size =2)
+
+ggplot(senate_network, aes(x=x, y=y, xend=xend, yend=yend))+
+  geom_edges(aes(linetype=line),color = 'grey50') +
+  theme_blank() +
+  geom_nodetext(aes(label =vertex.names), size = 3) +
+  geom_node_point(aes(fill=party)) +
+  scale_fill_manual(party, c("Democrat" = 'blue', "Republican" = "red", "Independent"="green"))
+  
+######################
+#  TRY AGAIN FOR SENATE
+######################
+edges_df %>% 
+  filter(source =='Benjamin Sasse')
+
+senate_graph <- network(senate_df[,c('source','target')])
+count <- network.edgecount(senate_graph)
+set.edge.attribute(senate_graph, "type", senate_df[,'type'])
+set.edge.attribute(senate_graph, 'line', senate_df[,'line'])
+set.edge.attribute(senate_graph, 'party', senate_df[,'party'])
+
+senate_gg <- ggnetwork(senate_graph,layout = "fruchtermanreingold", cell.jitter = 3)
+senate_gg$betweenness <- round(betweenness(senate_graph)[senate_gg$vertex.names],2)
+#View(senate_gg)
+ggplot(senate_gg, aes(x = x, y = y, xend=xend, yend=yend, color = party))  +
+  geom_edges(aes(linetype = line), curvature = .4, alpha=.5, size=.1) +
+  geom_nodes(alpha= .6, aes(fill =party, size =betweenness), show.legend = FALSE) +
+  geom_label_repel(aes(x=x, y=y,label=vertex.names), size =2) +
+  #geom_nodetext(aes(label = vertex.names), fontface = "bold", show.legend = FALSE) +
+  scale_color_manual(values=c('Democrat'='dodgerblue', 'Republican'='red', 'Independent'='forestgreen', 'NA'='grey'))+
+  theme_blank() +
+  scale_linetype_manual(values = c('solid'='solid', 'dotted'='dotted')) 
+
+ggiraph(code={print(plot)})
+
+?geom_edges()
+geom_labe
